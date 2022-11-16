8a57626cb88f6013dc4ef73f6e7b99a8f6eb7501
Suleiman <suleimanzhukov@gmail.com>
2022-11-16 21:26:05 +0200

    clean up

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,9 @@
 import React, { useState } from "react";
-import Dropdown from "react-bootstrap/Dropdown";
 import CustomDatepicker from "./common/datepicker";
 import CustomDropdown from "./common/dropdown";
 import TextInput from "./common/textInput";
+import { actionTypes, applicationTypes } from "../utils/dropdownData";
 import "../css/header.css";
-
-const data = [
-  "Employee Name",
-  "Action type",
-  "Application type",
-  "From Data",
-  "To Date",
-  "Application ID",
-];
-
-const actionTypes = [
-  { data: "DARI_REFRESH_TOKEN", label: "Dari Refresh Token" },
-  { data: "INITIATE_APPLICATION", label: "Initiate Application" },
-  { data: "SUBMIT_APPLICATION", label: "Submit Application" },
-  { data: "DARI_APP_LOGIN", label: "App Login" },
-];
-
-const applicationTypes = [
-  { data: "LEASE_REGISTRATION", label: "Lease Registration" },
-  { data: "CERT_TITLE_DEED_PLOT", label: "Cert Title Deed Plot" },
-  { data: "ADD_POA", label: "Add POA" },
-];
 
 const Header = ({ handleSearch }) => {
   const [name, setName] = useState("");
---
+++ src/utils/dropdownData.js
@@ -0,0 +1,12 @@
+export const actionTypes = [
+  { data: "DARI_REFRESH_TOKEN", label: "Dari Refresh Token" },
+  { data: "INITIATE_APPLICATION", label: "Initiate Application" },
+  { data: "SUBMIT_APPLICATION", label: "Submit Application" },
+  { data: "DARI_APP_LOGIN", label: "App Login" },
+];
+
+export const applicationTypes = [
+  { data: "LEASE_REGISTRATION", label: "Lease Registration" },
+  { data: "CERT_TITLE_DEED_PLOT", label: "Cert Title Deed Plot" },
+  { data: "ADD_POA", label: "Add POA" },
+];
